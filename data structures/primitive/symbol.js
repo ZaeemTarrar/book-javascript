3c7d3c91f49d564ee62cecedfdbf3f404491b92f
Zaeem Hassan Tarar <zaeemhassantarar@Zaeems-MBP-2.realtek>
2024-05-21 01:59:50 +0800

    DS

--- data structures/primitive/symbol.js
+++ data structures/primitive/symbol.js
@@ -1,8 +1,10 @@
 // Primitive Data-Type
 const Launch = () => {
   // Symbol
-  let s1 = Symbol();
-  console.log("S1: ", s1);
+  let s1 = Symbol("My Id");
+  console.log("S1 [Description]: ", s1.description);
+  console.log("S1 [String]: ", s1.toString());
+  console.log("S1 [Value]: ", s1.valueOf());
 };
 
 Launch();
