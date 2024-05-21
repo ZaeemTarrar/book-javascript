1c0fd3f05e655cf69d8e8b8a42e0312a5a4544d2
Zaeem Hassan Tarar <zaeemhassantarar@Zaeems-MBP-2.realtek>
2024-05-22 04:22:20 +0800

    DS

--- data structures/primitive/number.js
+++ data structures/primitive/number.js
@@ -1,55 +1,71 @@
-// Primitive Data-Type
-const Launch = () => {
-  // Number
-  let w = new Number(1.2321);
-  console.log("New Num Obj: ", w);
-  console.log("New Num Obj [Exp]: ", w.toExponential(3));
-  console.log("New Num Obj [Pre]: ", w.toPrecision(2));
-  console.log("New Num Obj [Fix]: ", w.toFixed(2));
+/**
+ * Primitive/Non-Primitive Data Type/Structure
+ * Name: Number
+ * Description: ...
+ * SubTypes:
+ *  Integer
+ *  Float/Double
+ *  Signed/Unsigned
+ *  Hex/Octal/Binary
+ *  Exponential/Decimal
+ *  NaN -> Not A Number
+ **/
 
-  let a = "24"; // Number String
-  let a2 = Number("35"); // Number String
-  let a3 = Number.parseInt("18"); // Number String
-  let a4 = Number.parseFloat("76.342"); // Number String
-  console.log("NumString1: ", +a);
-  console.log("NumString2: ", a2);
-  console.log("NumString3: ", a3);
-  console.log("NumString4: ", a4);
+let A = new Number(1.2321); // Object
+let B = 5; // Integer
+let C = +"24"; // String to Number (+)
+let D = Number("35"); // String to Number
+let E = Number.parseInt("18"); // to Integer
+let F = Number.parseFloat("76.342"); // to Float
+let G = -9; // Negative Integer
+let H = 7.8345; // Decimal
+let I = -4.867328; // Negative Decimal
+let J = 0xff; // Hex
+let K = 0b11111111; // Binary
+let L = 0o11111111; // Octal
+let M = 0.255e3; // Exponential
+let N = Infinity; // Infinity
+let O = Number("Hello"); // String to Number -> NaN
 
-  let x = 5; // Integer
-  let y = -9; // Negative
-  let z = 7.8345; // Decimal
+console.log("A: ", A);
+console.log("A [Exp]: ", w.toExponential(3));
+console.log("A [Pre]: ", w.toPrecision(2));
+console.log("A [Fix]: ", w.toFixed(2));
+console.log("B: ", B);
+console.log("C: ", C);
+console.log("D: ", D);
+console.log("E: ", E);
+console.log("F: ", F);
+console.log("G: ", G);
+console.log("H: ", H);
+console.log("I: ", I);
+console.log("J: ", J);
+console.log("K: ", K);
+console.log("L: ", L);
+console.log("M: ", M);
+console.log("N: ", N);
 
-  let sum1 = x + y;
-  let sum2 = x + z;
-  let sum3 = x + +a;
-  console.log("Sum1: ", sum1);
-  console.log("Sum2: ", sum2);
-  console.log("Sum3: ", sum3);
+// Arithmetics
+let s1 = A + B;
+let s2 = G + H;
+let s3 = I + +J;
 
-  let b = 0xff; // Hex
-  let c = 0b11111111; // Binary
-  let d = 0o11111111; // Octal
-  let e = 0.255e3; // Exponential
-  console.log("Hex: ", b);
-  console.log("Binary: ", c);
-  console.log("Octal: ", d);
-  console.log("Exponential: ", e);
+let p1 = L * M;
+let p2 = (M * H).toFixed(3);
 
-  let prod = e * b;
-  let prod2 = (e * z).toFixed(3);
-  console.log("Prod1: ", prod);
-  console.log("Prod2: ", prod2);
+console.log("S1: ", s1);
+console.log("S2: ", s2);
+console.log("S3: ", s3);
+console.log("P1: ", p1);
+console.log("P2: ", p2);
 
-  let PosNegSum = Number.POSITIVE_INFINITY + Number.NEGATIVE_INFINITY;
-  console.log("NumberMax: ", Number.MAX_SAFE_INTEGER);
-  console.log("PosMax: ", Number.POSITIVE_INFINITY);
-  console.log("NegMax: ", Number.NEGATIVE_INFINITY);
-  console.log("PosNegSum: ", PosNegSum);
+// Min Max Limits
+console.log("Min: ", Number.MIN_VALUE);
+console.log("Max: ", Number.MAX_VALUE);
+console.log("SafeMax: ", Number.MAX_SAFE_INTEGER);
+console.log("PosMax: ", Number.POSITIVE_INFINITY);
+console.log("NegMax: ", Number.NEGATIVE_INFINITY);
 
-  let f = Infinity;
-  let g = -Infinity;
-  console.log("Is Infinity: ", !Number.isFinite(f));
-};
-
-Launch();
+// Checks
+console.log("N Is Infinity: ", !Number.isFinite(N));
+console.log("O Is NaN: ", !Number.isNaN(O));
